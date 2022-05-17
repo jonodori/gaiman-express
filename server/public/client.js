d926801a92f69e9817134811ce717164b3e0c6f8
Jonathan Palalay <ring5pun@hotmail.com>
2022-05-17 16:57:01 -0500

    added games array

---
+++ server/public/client.js
@@ -0,0 +1,9 @@
+$(document).ready(handleReady)
+
+function handleReady(){
+    $('#main').append(`Test! 😃`);
+}
+
+console.log(`Look ma, I'm a client 🐸`);
+console.log('It works!');
+
--- server/server.js
+++ server/server.js
@@ -1,1 +1,42 @@
+// Import express 
+const express = require('express');
+
+// Create our express app 
+const app = express();
+
+let games = [
+    {name: 'Minecraft',
+     funLevel: 5  
+    },
+    {name: 'CoD',
+     funLevel: 6   
+    },
+    {name: 'Super Mario Bros',
+     funLevel: 7  
+    },
+    {name: 'Final Fantasy 7',
+     funLevel: 10
+    },
+];
+//                 request, response
+app.get('/games', (req, res) => {
+    // This code is run when someone asks to get games 
+    // ie. goes to http://localhost:5000/games 
+
+    // Send the games back to the client 
+    res.send(games);
+});
+
+
+
+// Share my client side files
+// with public (as in, anyone that wants them)
+app.use(express.static('server/public'));
+
+app.listen(5000, ()=> {
+    console.log('🌽 I\'m listening....');
+
+});
+
 console.log(`Look ma, I'm a server!`);
+
